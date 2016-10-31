3752077fed5b33344de65818ffbd0e229da074ce
noahrb <nbev0918@740s-pl-127.local>
2016-10-31 12:34:05 -0600

    added deck and playingcarddeck.swift files

---
+++ CardGame/Model/Deck.swift
@@ -0,0 +1,9 @@
+//
+//  Deck.swift
+//  CardGame
+//
+//  Created by Beverly, Noah on 10/31/16.
+//  Copyright © 2016 Beverly, Noah. All rights reserved.
+//
+
+import Foundation
---
+++ CardGame/Model/PlayingCardDeck.swift
@@ -0,0 +1,14 @@
+//
+//  PlayingCardDeck.swift
+//  CardGame
+//
+//  Created by Beverly, Noah on 10/31/16.
+//  Copyright © 2016 Beverly, Noah. All rights reserved.
+//
+
+import Foundation
+
+class PlayingCardDeck : Deck
+{
+    
+}
