454070c8ba7f5756ffdd49319389f49aa24e785a
noahrb <nbev0918@740s-pl-127.local>
2016-11-11 13:06:26 -0700

    Attempting to write cardChecker method
    not much success, building out basic fundamentals for this to happen:
    hand, slot, the checker code

--- CardGame/Model/StupidGame.swift
+++ CardGame/Model/StupidGame.swift
@@ -14,15 +14,35 @@ class StupidGame
     // Declaration Section
     //internal var nameOfVariable : Type
     internal var gameDeck : PlayingCardDeck
+    internal var slot1 : Int
+    internal var slot2 : Int
+    internal var matchChecker : Bool
+    
     // inits
     init()
     {
         gameDeck = PlayingCardDeck()
+        slot1 =
+        slot2 =
+        matchChecker = false
     }
     
     // Methods
     func startGame() -> Void
     {
-        
+         func matchChecker(sender: AnyObject) -> Void
+        {
+            // going to write this variable by using temp variables and comparing the 2 slots, if its a match -> remove from system.
+            if slot1.value = slot2.value -> for
+                
+                
+                while slot1.value = slot2.value
+                {
+                    let selectedCard = Int 
+            }
+            {
+                
+            }
+        }
     }
 }
