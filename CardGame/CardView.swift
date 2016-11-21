4edbf46defe416f7f0bad77e93331ab32b3c3b97
noahrb <nbev0918@740s-pl-127.local>
2016-11-21 13:54:14 -0700

    added CardView class

---
+++ CardGame/CardView.swift
@@ -0,0 +1,47 @@
+//
+//  CardView.swift
+//  CardGame
+//
+//  Created by Beverly, Noah on 11/21/16.
+//  Copyright © 2016 Beverly, Noah. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class CardView : UIView
+{
+    internal var frontImage : UIImageView
+    internal var backImage : UIImageView
+    @IBInspectable internal var isFaceUp : Bool
+    @IBInspectable internal var cornerCurve : CGFloat
+    internal var bottomLabel : UILabel
+    internal var topLabel : UILabel
+    
+    
+    init()
+    {
+        frontImage = UIImageView()
+        backImage = UIImageView()
+        isFaceUp = Bool()
+        bottomLabel = UILabel()
+        topLabel = UILabel()
+        cornerCurve = CGFloat()
+        super.init(frame: CGRect(x:0,y:0,width:400, height:800))
+    }
+
+    required init?(coder acoder:NSCoder))
+    {
+        frontImage = UIImageView()
+        backImage = UIImageView()
+        isFaceUp = Bool()
+        bottomLabel = UILabel()
+        topLabel = UILabel()
+        cornerCurve = CGFloat()
+        super.init(coder: acoder)
+    }
+    
+    func setup() -> Void
+    {
+        
+    }
+}
